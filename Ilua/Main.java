90e02197afa82e1e8310470fa1e7d70bc6afdb83
temich719 <87834061+temich719@users.noreply.github.com>
2021-09-19 17:51:59 +0300

    Delete Ilua directory

--- Ilua/Main.java
+++
@@ -1,8 +0,0 @@
-package Ilua;
-
-public class Main {//еще имена классов поменял тк они были не везде хороши, ну и методов
-    public static void main(String[] args) {
-        CPU.execute();
-    }
-}
-
